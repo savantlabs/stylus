f2c595518c803dbc57fdf943e2d5257d55521a1b
Yintao Song <yintaosong@users.noreply.github.com>
2023-02-19 15:53:23 +0800

    split mutex and data

--- sdk/src/main/java/io/savantlabs/stylus/sdk/jupyter/JupyterProcess.java
+++ sdk/src/main/java/io/savantlabs/stylus/sdk/jupyter/JupyterProcess.java
@@ -2,7 +2,6 @@ package io.savantlabs.stylus.sdk.jupyter;
 
 import java.net.URI;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import lombok.Getter;
@@ -20,8 +19,8 @@ public class JupyterProcess {
 
   @NonNull private Process process;
 
-  private final AtomicLong jupyterPID = new AtomicLong(-1);
-  private boolean stopPipe; // do not save messages in the queue
+  private final Object jupyterPIDMutex = new Object();
+  private long jupyterPID = -1;
   private Thread shutdownHook;
   private boolean stopped;
 
@@ -32,14 +31,13 @@ public class JupyterProcess {
             (p) ->
                 (line) -> {
                   log.info("jupyter process [{}]: {}", p.pid(), line);
-                  synchronized (jupyterPID) {
-                    if (jupyterPID.get() < 0) {
-                      Matcher matcher = JUPYTER_PID_PATTERN.matcher(line);
-                      if (matcher.matches()) {
-                        long pid = Long.parseLong(matcher.group("pid"));
-                        log.info("Found jupyter pid from console output: {}", pid);
-                        jupyterPID.set(pid);
-                        jupyterPID.notify();
+                  if (jupyterPID < 0) {
+                    Matcher matcher = JUPYTER_PID_PATTERN.matcher(line);
+                    if (matcher.matches()) {
+                      jupyterPID = Long.parseLong(matcher.group("pid"));
+                      log.info("Found jupyter pid from console output: {}", jupyterPID);
+                      synchronized (jupyterPIDMutex) {
+                        jupyterPIDMutex.notify();
                       }
                     }
                   }
@@ -53,9 +51,10 @@ public class JupyterProcess {
     log.info("Started the jupyter process {} ...", process.pid());
     shutdownHook = new Thread(this::stop);
     Runtime.getRuntime().addShutdownHook(shutdownHook);
-    // TODO: extract JUPYTER_PID from `pipe`, which is useful when stopping the process
-    synchronized (jupyterPID) {
-      jupyterPID.wait(TimeUnit.MINUTES.toMillis(1));
+    if (jupyterPID < 0) {
+      synchronized (jupyterPIDMutex) {
+        jupyterPIDMutex.wait(TimeUnit.MINUTES.toMillis(1));
+      }
     }
     log.info("Jupyter client is fully initialized.");
   }
@@ -67,11 +66,10 @@ public class JupyterProcess {
         Runtime.getRuntime().removeShutdownHook(shutdownHook);
         shutdownHook = null;
       }
-      if (jupyterPID.get() > 0) {
-        log.info("Tearing down the jupyter server {} ...", jupyterPID.get());
+      if (jupyterPID > 0) {
+        log.info("Tearing down the jupyter server {} ...", jupyterPID);
         Process process1 =
-            ShellRunner.runCommand(
-                "/bin/bash", "-c", String.format("kill -9 %d", jupyterPID.get()));
+            ShellRunner.runCommand("/bin/bash", "-c", String.format("kill -9 %d", jupyterPID));
         process1.waitFor(1, TimeUnit.MINUTES);
       }
       try {
