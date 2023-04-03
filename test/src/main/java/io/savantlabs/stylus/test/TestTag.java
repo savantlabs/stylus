8b01f5689adbd393961f7b721961d6754f0d26f7
Yintao Song <yintaosong@users.noreply.github.com>
2023-04-03 11:03:59 +0800

    boilerplate session service

--- test/src/main/java/io/savantlabs/stylus/test/TestTag.java
+++ test/src/main/java/io/savantlabs/stylus/test/TestTag.java
@@ -6,5 +6,6 @@ import lombok.experimental.UtilityClass;
 public class TestTag {
 
   public static final String UNIT = "unit";
+  public static final String FUNCTIONAL = "functional";
   public static final String MANUAL = "manual";
 }
