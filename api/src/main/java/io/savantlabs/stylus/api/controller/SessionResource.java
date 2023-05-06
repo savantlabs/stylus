91f4f910eeeb2bf5d4af72cfdc755d82f6a7308a
Shivam Bodiwala <shivambodiwala@Shivams-MacBook-Pro.local>
2023-05-05 19:40:48 -0700

    Updated Controller and Service

--- api/src/main/java/io/savantlabs/stylus/api/controller/SessionResource.java
+++ api/src/main/java/io/savantlabs/stylus/api/controller/SessionResource.java
@@ -4,8 +4,10 @@ import io.savantlabs.stylus.api.model.Session;
 import io.savantlabs.stylus.api.service.SessionService;
 import jakarta.inject.Inject;
 import java.util.List;
+import java.util.concurrent.ConcurrentMap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,15 +20,24 @@ public class SessionResource {
 
   @Inject private SessionService sessionService;
 
-  @GetMapping
+  @GetMapping("/listSessions")
   @ResponseBody
   List<Session> listSessions() {
     return sessionService.listSessions();
   }
 
-  @PostMapping
+  @PostMapping("/startSession")
   @ResponseBody
   Session startSession() {
     return sessionService.startSession();
   }
+  @GetMapping("/getSession/{sessionId}")
+  @ResponseBody
+  Session getSession(@PathVariable(value = "sessionId") String sessionId) { return sessionService.getSession(sessionId);}
+  @PostMapping("/stopSession/{sessionId}")
+  @ResponseBody
+  void stopSession(@PathVariable(value = "sessionId") String sessionId) {sessionService.stopSession(sessionId);}
+  @GetMapping("/startSessionKernel/{sessionId}")
+  @ResponseBody
+  Object startSessionKernel(@PathVariable(value = "sessionId") String sessionId) throws InterruptedException {return sessionService.startSessionKernel(sessionId);}
 }
--- api/src/test/java/io/savantlabs/stylus/api/service/impl/SessionServiceImplTests.java
+++ api/src/test/java/io/savantlabs/stylus/api/service/impl/SessionServiceImplTests.java
@@ -1,20 +1,53 @@
 package io.savantlabs.stylus.api.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.savantlabs.stylus.api.framework.FunctionalTestsBase;
+import io.savantlabs.stylus.api.model.Session;
 import io.savantlabs.stylus.api.service.SessionService;
-import io.savantlabs.stylus.sdk.jupyter.JupyterClient;
+import io.savantlabs.stylus.sdk.jupyter.ShellRunner;
 import io.savantlabs.stylus.test.TestTag;
 import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
+@Slf4j
 public class SessionServiceImplTests extends FunctionalTestsBase {
 
   @Inject private SessionService sessionService;
 
   @Test
   @Tag(TestTag.FUNCTIONAL)
-  void testStartSessionAndKernel() {
-    JupyterClient client = ((SessionServiceImpl) sessionService).getJupyterClientForTest();
+  void testStartSessionAndKernel() throws Exception {
+//  JupyterClient client = ((SessionServiceImpl) sessionService).getJupyterClientForTest();
+    Session session = sessionService.startSession();
+    // Verifying that the session's ID is not null
+    assertNotNull(session.getId());
+    // Verifying that the session's kernelId is initially an empty string
+    assertEquals("",session.getKernelId());
+    // Starting a kernel for the session
+    Object kernel = sessionService.startSessionKernel(session.getId());
+    // Verifying that the session's kernelId is not null
+    assertNotNull(session.getKernelId());
   }
+
+
+//  @Test
+//  @Tag(TestTag.FUNCTIONAL)
+//  void apiTestingStartSession() throws Exception {
+//    StylusApplication.main(new String[]{});
+//
+//    ApiProxy proxy = getApiProxy();
+//    JsonNode res = proxy.get(getRestEndpoint("/startSession"), JsonNode.class);
+//    System.out.println("resp: {}"+res); // to be removed
+//
+//    mockMvc.perform(get("/api/startSession"))
+//        .andExpect(status().isOk());
+//  }
 }
+
